b0071833eda0e62fbdea0967782049140e00bbad
Saleem <mojowhar@paypal.com>
2024-05-16 15:38:22 +0530

    adding code

--- dashboard/starter-example/app/dashboard/invoices/page.tsx
+++ dashboard/starter-example/app/dashboard/invoices/page.tsx
@@ -1,4 +1,9 @@
-export default function Page() {
+import { unstable_noStore as noStore } from "next/cache";
+
+
+export default async function Page() {
+  noStore();
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
     <p>Invoices Page</p>
   )
--- dashboard/starter-example/app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex w-100 text-xl">Dashboard Page</div>
-  )
-}
